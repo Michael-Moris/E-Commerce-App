a96b54c695dff8636f8a6e963cc04a9b0fd23f0e
Michael-Moris <michaelmorisdev@gmail.com>
2025-02-10 02:35:38 +0200

    Set up initial project structure

---
+++ src/app/features/product/components/product-detalis/product-detalis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detalis',
+  imports: [],
+  templateUrl: './product-detalis.component.html',
+  styleUrl: './product-detalis.component.css'
+})
+export class ProductDetalisComponent {
+
+}
