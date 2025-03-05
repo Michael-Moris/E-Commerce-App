45046fedfec565c98837292186b83617a1f58c4d
Michael-Moris <michaelmorisdev@gmail.com>
2025-03-05 05:41:59 +0200

    fix: prevent infinite loading by checking authToken in localStorage

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -1,15 +1,22 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { finalize } from 'rxjs';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
 
   const spinner = inject(NgxSpinnerService)
+  const plat_id = inject(PLATFORM_ID)
 
-  spinner.show()
+  if (isPlatformBrowser(plat_id)) {
+    if (localStorage.getItem('authToken') !== null) {
+      spinner.show()
+    }
+  }
 
   return next(req).pipe(finalize(() => {
     spinner.hide()
   }))
 };
+
