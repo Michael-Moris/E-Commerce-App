e7138852701008e67d919e2ab8874d690e84d001
Michael-Moris <michaelmorisdev@gmail.com>
2025-03-10 21:34:18 +0200

    feat: implement pagination in product list.
    - Updated product service to accept page number as a parameter
    - Modified product list component to fetch paginated data
    - Integrated ngx-pagination controls in the template
    - Handled API metadata (limit, currentPage, totalItems)

--- src/app/features/product/components/product-list/product-list.component.ts
+++ src/app/features/product/components/product-list/product-list.component.ts
@@ -4,19 +4,23 @@ import { ProductsService } from './../../services/products.service';
 import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from "../product-card/product-card.component";
 import { ToastrService } from 'ngx-toastr';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, NgxPaginationModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
   allProducts: Product[] = [];
+  pageSize: number = 0;
+  currentPage: number = 1;
+  total: number = 0;
 
   private readonly ProductsService = inject(ProductsService);
   private readonly CartService = inject(CartService);
-  private readonly toastr = inject(ToastrService)
+  private readonly toastr = inject(ToastrService);
 
   showToastr(msg: string) {
     this.toastr.success(msg, '', {
@@ -25,26 +29,38 @@ export class ProductListComponent {
     });
   }
 
-
-  getAllProcuts() {
+  getAllProducts() {
     this.ProductsService.getProducts().subscribe({
-      next: ({ data }) => {
-        this.allProducts = data;
+      next: (res) => {
+        this.allProducts = res.data;
+        this.pageSize = res.metadata.limit;
+        this.currentPage = res.metadata.currentPage;
+        this.total = res.results;
       }
-    })
+    });
   }
-
 
   addProductToCart(id: string) {
     this.CartService.addProductToCart(id).subscribe({
       next: (res) => {
-        this.showToastr('Product Added Successfully')
-        this.CartService.cartCounter.set(res.numOfCartItems)
+        this.showToastr('Product Added Successfully');
+        this.CartService.cartCounter.set(res.numOfCartItems);
+      }
+    });
+  }
+
+  pageChanged(event: number): void {
+    this.ProductsService.getProducts(event).subscribe({
+      next: (res) => {
+        this.allProducts = res.data;
+        this.pageSize = res.metadata.limit;
+        this.currentPage = res.metadata.currentPage;
+        this.total = res.results;
       }
     });
   }
 
   ngOnInit(): void {
-    this.getAllProcuts();
+    this.getAllProducts();
   }
 }
