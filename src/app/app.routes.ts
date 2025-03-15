1529702940a3cf4c91054e71a8523eede8b21ac8
Michael-Moris <michaelmorisdev@gmail.com>
2025-03-15 14:37:04 +0200

    feat: Implement lazy loading for routes and enhance navbar functionality in main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,45 +1,37 @@
-import { LoginComponent } from './core/auth/components/login/login.component';
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './core/auth/components/register/register.component';
-import { HomeComponent } from './features/home/components/home/home.component';
-import { NotFoundComponent } from './core/auth/components/not-found/not-found.component';
-import { AuthLayoutComponent } from './core/layouts/auth/auth-layout/auth-layout/auth-layout.component';
-import { MainLayoutComponent } from './core/layouts/auth/main-layout/main-layout/main-layout.component';
-import { ProductListComponent } from './features/product/components/product-list/product-list.component';
-import { BrandListComponent } from './features/brands/components/brand-list/brand-list.component';
-import { ProductDetalisComponent } from './features/product/components/product-detalis/product-detalis.component';
 import { authGuard } from './core/guards/auth.guard';
 import { isLoggedGuard } from './core/guards/is-logged.guard';
-import { CartListComponent } from './features/cart/components/cart-list/cart-list.component';
-import { CheckoutComponent } from './features/orders/components/checkout/checkout.component';
-import { OrdersComponent } from './features/orders/components/orders/orders.component';
-import { ForgotPasswordComponent } from './core/auth/components/forgot-password/forgot-password.component';
-import { CategoriesListComponent } from './features/category/components/categories-list/categories-list.component';
-import { WishListListComponent } from './features/wishList/components/wish-list-list/wish-list-list.component';
+import { AuthLayoutComponent } from './core/layouts/auth/auth-layout/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './core/layouts/auth/main-layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
     {
-        path: '', component: AuthLayoutComponent, canActivate: [isLoggedGuard], children: [
+        path: '',
+        component: AuthLayoutComponent,
+        canActivate: [isLoggedGuard],
+        children: [
             { path: '', redirectTo: 'login', pathMatch: 'full' },
-            { path: 'login', component: LoginComponent, title: 'Login' },
-            { path: 'register', component: RegisterComponent, title: 'Register' },
-            { path: 'forgotpassword', component: ForgotPasswordComponent, title: 'Forgot Password' },
+            { path: 'login', loadComponent: () => import('./core/auth/components/login/login.component').then((c) => c.LoginComponent) },
+            { path: 'register', loadComponent: () => import('./core/auth/components/register/register.component').then((c) => c.RegisterComponent), title: 'Register' },
+            { path: 'forgotpassword', loadComponent: () => import('./core/auth/components/forgot-password/forgot-password.component').then((c) => c.ForgotPasswordComponent), title: 'Forgot Password' },
         ]
     },
     {
-        path: '', component: MainLayoutComponent, canActivate: [authGuard], children: [
+        path: '',
+        component: MainLayoutComponent,
+        canActivate: [authGuard],
+        children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent, title: 'Home' },
-            { path: 'categories', component: CategoriesListComponent, title: 'Categories' },
-            { path: 'products', component: ProductListComponent, title: 'Products' },
-            { path: 'product-detalis/:id', component: ProductDetalisComponent, title: 'Product Detalis' },
-            { path: 'brands', component: BrandListComponent, title: 'Brands' },
-            { path: 'cart', component: CartListComponent, title: 'Cart' },
-            { path: 'wishlist', component: WishListListComponent, title: 'WishList' },
-            { path: 'checkout/:id', component: CheckoutComponent, title: 'checkout' },
-            { path: 'allorders', component: OrdersComponent, title: 'My Orders' },
-            { path: '**', component: NotFoundComponent, title: 'Not Found' }
+            { path: 'home', loadComponent: () => import('./features/home/components/home/home.component').then((c) => c.HomeComponent), title: 'Home' },
+            { path: 'categories', loadComponent: () => import('./features/category/components/categories-list/categories-list.component').then((c) => c.CategoriesListComponent), title: 'Categories' },
+            { path: 'products', loadComponent: () => import('./features/product/components/product-list/product-list.component').then((c) => c.ProductListComponent), title: 'Products' },
+            { path: 'product-detalis/:id', loadComponent: () => import('./features/product/components/product-detalis/product-detalis.component').then((c) => c.ProductDetalisComponent), title: 'Product Detalis' },
+            { path: 'brands', loadComponent: () => import('./features/brands/components/brand-list/brand-list.component').then((c) => c.BrandListComponent), title: 'Brands' },
+            { path: 'cart', loadComponent: () => import('./features/cart/components/cart-list/cart-list.component').then((c) => c.CartListComponent), title: 'Cart' },
+            { path: 'wishlist', loadComponent: () => import('./features/wishList/components/wish-list-list/wish-list-list.component').then((c) => c.WishListListComponent), title: 'WishList' },
+            { path: 'checkout/:id', loadComponent: () => import('./features/orders/components/checkout/checkout.component').then((c) => c.CheckoutComponent), title: 'Checkout' },
+            { path: 'allorders', loadComponent: () => import('./features/orders/components/orders/orders.component').then((c) => c.OrdersComponent), title: 'My Orders' },
+            { path: '**', loadComponent: () => import('./core/auth/components/not-found/not-found.component').then((c) => c.NotFoundComponent), title: 'Not Found' }
         ]
-    },
-
+    }
 ];
