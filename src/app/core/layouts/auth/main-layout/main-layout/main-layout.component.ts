36fda4d0a5ed2737514f4f9974f9b1421782ba7b
Michael-Moris <michaelmorisdev@gmail.com>
2025-03-16 13:56:07 +0200

    feat(main-layout): add scroll-to-top button with visibility based on scroll position.

--- src/app/core/layouts/auth/main-layout/main-layout/main-layout.component.ts
+++ src/app/core/layouts/auth/main-layout/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../../../../shared/components/navbar/navbar.component';
 import { FooterComponent } from '../../../../../shared/components/footer/footer.component';
@@ -8,5 +8,18 @@ import { FooterComponent } from '../../../../../shared/components/footer/footer.
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
-export class MainLayoutComponent  {
+export class MainLayoutComponent {
+  showBtn: boolean = false
+
+  goToTop() {
+    scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  @HostListener('window:scroll') scrollToTop() {
+    let scrollTop = document.documentElement.scrollTop
+    if (scrollTop > 500) {
+      this.showBtn = true
+    } else {
+      this.showBtn = false
+    }
+  }
 }
