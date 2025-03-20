0c57cbc61a375727a1526c7d21542681ef0933ab
Michael-Moris <michaelmorisdev@gmail.com>
2025-03-21 01:08:41 +0200

    feat(routes): add client render mode for product details and checkout routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,13 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'product-detalis/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Client
   }
 ];
