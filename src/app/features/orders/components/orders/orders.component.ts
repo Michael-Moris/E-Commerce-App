f5d4f92c547488a73b206301b0f56d96a05ebf67
Michael-Moris <michaelmorisdev@gmail.com>
2025-03-22 20:21:06 +0200

    Enhance orders UI: loading state, empty orders message, and navigation button.
    - Implemented loading state logic in the orders component to improve user experience during data fetch.
    - Added an empty orders state with a clear message, supportive illustration, and a call-to-action button that navigates users to /products.
    - Added an empty orders image to visually support the empty state UX.

--- src/app/features/orders/components/orders/orders.component.ts
+++ src/app/features/orders/components/orders/orders.component.ts
@@ -3,11 +3,12 @@ import { OrderService } from '../../services/order.service';
 import { AuthService } from '../../../../core/auth/services/auth.service';
 import { AllOrders } from '../../models/all-orders';
 import { DatePipe } from '@angular/common';
-import {NgxPaginationModule} from 'ngx-pagination';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-orders',
-  imports: [DatePipe ,NgxPaginationModule],
+  imports: [DatePipe, NgxPaginationModule, RouterLink],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
@@ -19,16 +20,20 @@ export class OrdersComponent implements OnInit {
   userId: string = ''
   allOrdersList: AllOrders[] = []
   p: number = 1;
+  isLoading: boolean = false
 
   ngOnInit(): void {
+    this.isLoading = true
     this.authService.saveUserData();
     this.userId = this.authService.userData.id
     this.orderService.getUserOrders(this.userId).subscribe({
       next: (res) => {
         this.allOrdersList = res
+        this.isLoading = false
       },
       error: (err) => {
         console.log(err);
+        this.isLoading = false
       }
     })
   }
